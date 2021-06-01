cc7f6b09d2fff70300f04ea6a3035fee1a953762
Paddy <zefiru@protonmail.com>
2021-06-01 20:06:46 +0100

    Add hero buttons

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-// 											Variables 											//
-// Menu Icon //
+// // 											Variables 											//
+// // Menu Icon //
 const menuIcon = document.querySelector(".hamburger-menu");
 const menuItems = document.querySelector(".menu-items");
 let isMenuOpen = false;
 
-// Functions
+// // Functions
 const openMenu = () => {
 	menuIcon.classList.toggle("open-menu");
 	isMenuOpen = !isMenuOpen;
@@ -13,16 +13,16 @@ const openMenu = () => {
 		: ((menuItems.style.opacity = 0), (menuItems.style.zIndex = -1));
 };
 
-const backgroundParallax = () => {
-	let backgroundImage = document.querySelector(".hero");
-	let speed = 10;
-	backgroundImage.addEventListener("mousemove", ({ pageX, pageY }) => {
-		let x = (window.innerWidth - pageX * speed) / 100;
-		let y = (window.innerWidth - pageY * speed) / 100;
-		backgroundImage.style.transform = `translate(${x}px, ${y}px)`;
-	});
-};
+// const backgroundParallax = () => {
+// 	let backgroundImage = document.querySelector(".hero-background");
+// 	let speed = 3;
+// 	window.addEventListener("mousemove", ({ pageX, pageY }) => {
+// 		let x = (window.innerWidth - pageX * speed) / 100;
+// 		let y = (window.innerWidth - pageY * speed) / 100;
+// 		backgroundImage.style.transform = `translate(${x}px, ${y}px)`;
+// 	});
+// };
 
-// Execute functions
-backgroundParallax();
+// // Execute functions
+// backgroundParallax();
 menuIcon.addEventListener("click", openMenu);
