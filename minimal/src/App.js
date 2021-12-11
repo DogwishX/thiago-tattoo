cedcc5db8517c947dd45916cce53d1d5d6067c0f
DogwishX <zefiru@protonmail.com>
2021-12-11 14:35:42 +0000

    Add carousel loop

--- minimal/src/App.js
+++ minimal/src/App.js
@@ -2,12 +2,16 @@ import "./App.css";
 import Gallery from "./Components/Gallery/Gallery";
 
 const images = [
+  "images/5.jpg",
+  "images/6.jpg",
   "images/1.jpg",
   "images/2.jpg",
   "images/3.jpg",
   "images/4.jpg",
   "images/5.jpg",
   "images/6.jpg",
+  "images/1.jpg",
+  "images/2.jpg",
 ];
 
 function App() {
--- minimal/src/Components/Gallery/Gallery.js
+++ minimal/src/Components/Gallery/Gallery.js
@@ -1,22 +1,48 @@
 import "./Gallery.css";
 
 function Gallery({ images }) {
-  function focusImage({ currentTarget }) {
+  function handleClick({ currentTarget }) {
+    loopCarousel(currentTarget);
+  }
+
+  function loopCarousel(currentTarget) {
+    const imgDivs = document.querySelectorAll(".gallery__image");
+    const currentImgId = Number(currentTarget.id);
+
+    if (currentImgId === 1) {
+      changeFocus(currentImgId, imgDivs);
+      return setTimeout(
+        () => changeFocus(imgDivs.length - 3, imgDivs, false),
+        450
+      );
+    }
+    if (currentImgId === imgDivs.length - 2) {
+      changeFocus(currentImgId, imgDivs);
+      return setTimeout(() => changeFocus(2, imgDivs, false), 500);
+    }
+
+    return changeFocus(currentImgId, imgDivs);
+  }
+
+  function changeFocus(divId, imgDivs, transition = true) {
+    removeFocusFromImgs(imgDivs);
+    focusImage(imgDivs[divId], transition);
+  }
+
+  function focusImage(currentTarget, transition) {
     const middleOfScreen = window.innerWidth / 2;
     const imgOffset =
       -(currentTarget.width * currentTarget.id) +
       middleOfScreen -
       currentTarget.width / 2;
-      
-    const transformArgs = `transform: translateX(${imgOffset}px);`;
-
-    removeFocusFromImgs();
+    const transformArgs = `transform: translateX(${imgOffset}px); transition: ${
+      transition ? `all 0.4s ease-in-out` : "none"
+    }`;
     currentTarget.classList.add("focused");
     currentTarget.parentNode.style = transformArgs;
   }
 
-  function removeFocusFromImgs() {
-    const imgDivs = document.querySelectorAll(".gallery__image");
+  function removeFocusFromImgs(imgDivs) {
     imgDivs.forEach((img) => img.classList.remove("focused"));
   }
 
@@ -30,7 +56,7 @@ function Gallery({ images }) {
           data-testid="gallery__image"
           src={src}
           alt={`tattoo ${index + 1}`}
-          onClick={focusImage}
+          onClick={handleClick}
         />
       ))}
     </div>
