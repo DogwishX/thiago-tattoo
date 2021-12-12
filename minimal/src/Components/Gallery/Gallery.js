8fdbd0e1c922e074e6b01e5af3307d75ae2c7ca2
DogwishX <zefiru@protonmail.com>
2021-12-12 12:24:27 +0000

    Add automatic scroll through gallery

--- minimal/src/Components/Gallery/Gallery.js
+++ minimal/src/Components/Gallery/Gallery.js
@@ -1,24 +1,43 @@
+import { useState, useEffect } from "react";
 import "./Gallery.css";
 
 function Gallery({ images }) {
+  const [focusedImg, setFocusedImg] = useState(3);
+  useEffect(() => {
+    setTimeout(() => {
+      const imgDivs = document.querySelectorAll(".gallery__image");
+      if (focusedImg === imgDivs.length - 2) setFocusedImg(3);
+      else if (focusedImg === 1) setFocusedImg(imgDivs.length - 2);
+      else setFocusedImg(focusedImg + 1);
+      loopCarousel(imgDivs[focusedImg]);
+      console.log(focusedImg);
+    }, 3000);
+  }, [focusedImg]);
+
   function handleClick({ currentTarget }) {
+    clearTimeout();
+    setFocusedImg(currentTarget.id);
     loopCarousel(currentTarget);
   }
 
   function loopCarousel(currentTarget) {
     const imgDivs = document.querySelectorAll(".gallery__image");
     const currentImgId = Number(currentTarget.id);
+    const loopImgOffset = 22;
 
     if (currentImgId === 1) {
       changeFocus(currentImgId, imgDivs);
       return setTimeout(
-        () => changeFocus(imgDivs.length - 3, imgDivs, false, -22),
-       420
+        () => changeFocus(imgDivs.length - 3, imgDivs, false, -loopImgOffset),
+        400
       );
     }
     if (currentImgId === imgDivs.length - 2) {
       changeFocus(currentImgId, imgDivs);
-      return setTimeout(() => changeFocus(2, imgDivs, false, 22), 420);
+      return setTimeout(
+        () => changeFocus(2, imgDivs, false, loopImgOffset),
+        400
+      );
     }
 
     return changeFocus(currentImgId, imgDivs);
@@ -35,9 +54,9 @@ function Gallery({ images }) {
       -(currentTarget.width * currentTarget.id) +
       middleOfScreen -
       currentTarget.width / 2;
-    const transformArgs = `transform: translateX(${transition? imgOffset : imgOffset + additionalOffset}px); transition: ${
-      transition ? `all 0.4s ease-in-out` : "none"
-    }`;
+    const transformArgs = `transform: translateX(${
+      transition ? imgOffset : imgOffset + additionalOffset
+    }px); transition: ${transition ? `all 0.4s ease-in-out` : "none"}`;
     currentTarget.classList.add("focused");
     currentTarget.parentNode.style = transformArgs;
   }
