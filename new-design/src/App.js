e00aa1ffc0c62a0360cc2a65cc271cba17364d33
DogwishX <zefiru@protonmail.com>
2022-02-14 03:27:08 +0000

    Add background img

--- new-design/src/App.js
+++ new-design/src/App.js
@@ -1,70 +1,10 @@
 import "./App.css";
-import Nav from "./Components/Nav";
+import Hero from "./Components/Hero";
 
 function App() {
   return (
     <div className="App">
-      <Nav />
-      <p>
-        Our bodies were printed as blank pages to be filled with the ink of our
-        hearts Michael Biondi Our Custom Tattoo Studio strives to be one of the
-        top tattoo shops in Chicago and bring you some of the city’s finest
-        tattoo artist. At Ink and Iron, we specialize in all tattoo styles; you
-        will definitely find what you are looking for. Our artist have brought
-        Chicago some of the most cutting edge tattoos. Our team of expert tattoo
-        artists brings their unique perspectives to our studio. We enjoy giving
-        our clients the work of art they are dreaming of. Whether it is your
-        first tattoo or another piece to add your canvas, Ink and Iron Custom
-        Tattoo Studio can fulfill all of your tattoo needs. We also do some of
-        the best cover-ups in the city of Chicago and our touch ups and custom
-        artwork are unparalleled. Come check out Ink and Iron Custom Tattoo
-        Studio and see the magnificent work our team of artists has done over
-        the years. You can be assured that you will be tattooed under the
-        cleanest environment available. Our relaxed facility is clean, friendly
-        and we invite you to come hang out. Ink and Iron Custom Tattoo Studio
-        wants to make you feel as comfortable as possible, listen to great
-        music, watch a movie on one of the four 42”flat screens and relax while
-        one of our artists gives you the piece of artwork that you have always
-        wanted. Our bodies were printed as blank pages to be filled with the ink
-        of our hearts Michael Biondi Our Custom Tattoo Studio strives to be one
-        of the top tattoo shops in Chicago and bring you some of the city’s
-        finest tattoo artist. At Ink and Iron, we specialize in all tattoo
-        styles; you will definitely find what you are looking for. Our artist
-        have brought Chicago some of the most cutting edge tattoos. Our team of
-        expert tattoo artists brings their unique perspectives to our studio. We
-        enjoy giving our clients the work of art they are dreaming of. Whether
-        it is your first tattoo or another piece to add your canvas, Ink and
-        Iron Custom Tattoo Studio can fulfill all of your tattoo needs. We also
-        do some of the best cover-ups in the city of Chicago and our touch ups
-        and custom artwork are unparalleled. Come check out Ink and Iron Custom
-        Tattoo Studio and see the magnificent work our team of artists has done
-        over the years. You can be assured that you will be tattooed under the
-        cleanest environment available. Our relaxed facility is clean, friendly
-        and we invite you to come hang out. Ink and Iron Custom Tattoo Studio
-        wants to make you feel as comfortable as possible, listen to great
-        music, watch a movie on one of the four 42”flat screens and relax while
-        one of our artists gives you the piece of artwork that you have always
-        wanted. Our bodies were printed as blank pages to be filled with the ink
-        of our hearts Michael Biondi Our Custom Tattoo Studio strives to be one
-        of the top tattoo shops in Chicago and bring you some of the city’s
-        finest tattoo artist. At Ink and Iron, we specialize in all tattoo
-        styles; you will definitely find what you are looking for. Our artist
-        have brought Chicago some of the most cutting edge tattoos. Our team of
-        expert tattoo artists brings their unique perspectives to our studio. We
-        enjoy giving our clients the work of art they are dreaming of. Whether
-        it is your first tattoo or another piece to add your canvas, Ink and
-        Iron Custom Tattoo Studio can fulfill all of your tattoo needs. We also
-        do some of the best cover-ups in the city of Chicago and our touch ups
-        and custom artwork are unparalleled. Come check out Ink and Iron Custom
-        Tattoo Studio and see the magnificent work our team of artists has done
-        over the years. You can be assured that you will be tattooed under the
-        cleanest environment available. Our relaxed facility is clean, friendly
-        and we invite you to come hang out. Ink and Iron Custom Tattoo Studio
-        wants to make you feel as comfortable as possible, listen to great
-        music, watch a movie on one of the four 42”flat screens and relax while
-        one of our artists gives you the piece of artwork that you have always
-        wanted.
-      </p>
+      <Hero />
     </div>
   );
 }
