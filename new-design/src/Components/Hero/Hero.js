50abb209b8073bdf43f22b4d66b20d00258e2b8a
DogwishX <zefiru@protonmail.com>
2022-02-20 01:41:59 +0000

    Add hero__title, hero__subtitle and hero__arrow with animation

--- new-design/src/Components/Hero/Hero.js
+++ new-design/src/Components/Hero/Hero.js
@@ -1,8 +1,17 @@
 import React from "react";
+import Title from "../Core/Title";
+import Subtitle from "../Core/Subtitle";
+import ArrowDown from "../ArrowDown";
 import "./Hero.css";
 
 function Hero() {
-  return <div className="hero">Hero</div>;
+  return (
+    <div className="hero">
+      <Title id="hero__title">thiago Tattoo</Title>
+      <Subtitle id="hero__subtitle">custom tattoos</Subtitle>
+      <ArrowDown id="hero__arrow" />
+    </div>
+  );
 }
 
 export default Hero;
