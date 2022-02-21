6d884900b705d15335166de8341769db611df015
DogwishX <zefiru@protonmail.com>
2022-02-21 02:53:04 +0000

    Add AboutMe

--- new-design/src/App.js
+++ new-design/src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
+import AboutMe from "./Components/AboutMe";
 import Hero from "./Components/Hero";
 
 function App() {
   return (
     <div className="App">
       <Hero />
+      <AboutMe />
+      <hr></hr>
     </div>
   );
 }
