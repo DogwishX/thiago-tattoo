b28fea49bda54b807f926db7d6bdfc5c84bf8d66
DogwishX <zefiru@protonmail.com>
2022-03-02 02:20:18 +0000

    Add useEffect to apply offset to gallery__viewport

--- new-design/src/Components/Gallery/Gallery.js
+++ new-design/src/Components/Gallery/Gallery.js
@@ -1,10 +1,17 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Gallery.css";
 
 function Gallery({ imgs }) {
   const [isMouseDown, setIsMouseDown] = useState(false);
-  const [positions, setPositions] = useState({ offset: 0, clientX: 0 });
-  const [focusedImg, setFocusedImg] = useState(2);
+  const [positions, setPositions] = useState({ offsetLeft: 0, clientX: 0 });
+  const [focusedImg, setFocusedImg] = useState(3);
+  const [hasImgChanged, setHasImageChanged] = useState(false);
+
+  useEffect(() => {
+    const galleryViewportDiv = document.querySelector(".gallery__viewport");
+
+    galleryViewportDiv.style = `transform: translateX(-${positions.offsetLeft}px)`;
+  });
 
   return (
     <div
@@ -29,21 +36,42 @@ function Gallery({ imgs }) {
 
   function handleMouseDown({ currentTarget, clientX }) {
     setIsMouseDown(true);
-    setPositions({ offsetLeft: currentTarget.offsetLeft, clientX });
+    setPositions({ ...positions, clientX });
   }
 
   function handleMouseMove({ target, currentTarget, clientX }) {
     const distanceX = clientX - positions.clientX;
-    const offsetLeft =
-      positions.offsetLeft + (distanceX > 0 ? -target.width : +target.width);
-
-    if (isMouseDown) {
-      currentTarget.style = `transform: translateX(${offsetLeft}px)`;
+    if (isMouseDown && !hasImgChanged) {
+      distanceX > 0 ? changeFocusedImg(+1) : changeFocusedImg(-1);
     }
   }
 
   function handleMouseUp() {
     setIsMouseDown(false);
+    setHasImageChanged(false);
+  }
+
+  function changeFocusedImg(addOrSubtract) {
+    // Validation of focusedImg values
+    if (
+      focusedImg - addOrSubtract < 0 ||
+      focusedImg + addOrSubtract >= imgs.length
+    ) {
+      return;
+    }
+
+    setFocusedImg(focusedImg + addOrSubtract);
+
+    // Handle gallery__viewport offset
+    const positionsClone = { ...positions };
+    const imagesNodeList = document.querySelectorAll(".gallery__image");
+    const focusedImgWidth = imagesNodeList[focusedImg].width;
+    positionsClone.offsetLeft =
+      addOrSubtract * focusedImgWidth + positions.offsetLeft;
+    setPositions(positionsClone);
+
+    // // Exit function
+    setHasImageChanged(true);
   }
 }
 
